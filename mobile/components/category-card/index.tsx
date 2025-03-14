3eae0d083e724fc132ae29e792fc45c324e045d9
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2025-03-14 16:54:46 +0100

    feat: integrate FAB component for adding new todos

---
+++ mobile/components/category-card/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+interface CategoryCardProps {
+  title: string;
+  count: number;
+  icon: React.ReactNode;
+  className?: string;
+  bgClassName: string;
+}
+
+const CategoryCard = ({
+  title,
+  count,
+  icon,
+  className,
+  bgClassName,
+}: CategoryCardProps) => {
+  return (
+    <View
+      className={`${bgClassName} h-[93px] w-[48%] rounded-md justify-center gap-5 p-5 ${className}`}
+    >
+      {icon}
+
+      <Text className="font-isemibold text-[17px]">
+        {count}{" "}
+        <Text className="font-imedium text-[#121212] opacity-[60%] capitalize">
+          {title}
+        </Text>
+      </Text>
+    </View>
+  );
+};
+
+export default CategoryCard;
