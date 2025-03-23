b2744d9f739d20294fcce71d8597f1187b0af0ba
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2025-03-23 04:56:31 +0100

    feat: implement CachedTodoRepository methods

--- api/Features/Todos/Repository/CachedTodoRepository.cs
+++ api/Features/Todos/Repository/CachedTodoRepository.cs
@@ -1,7 +1,7 @@
-using api.Application.Common.CategoryDetails;
-using api.Application.Common.PaginatedResult;
 using api.Domain.Entities.Todo;
 using api.Infrastructure.Services.Caching;
+using api.Application.Common.CategoryDetails;
+using api.Application.Common.PaginatedResult;
 
 namespace api.Features.Todos.Repository;
 
@@ -9,7 +9,6 @@ public class CachedTodoRepository : ITodoRepository
 {
     private readonly ITodoRepository _innerRepository;
     private readonly IRedisCacheService _cache;
-    private readonly TimeSpan _cacheDuration = TimeSpan.FromMinutes(10);
 
 
     public async Task<Todo?> CreateAsync(Todo entity)
@@ -19,12 +18,35 @@ public class CachedTodoRepository : ITodoRepository
 
     public async Task<Todo?> GetByIdAsync(Guid id)
     {
-        return await _innerRepository.GetByIdAsync(id);
+        var cacheKey = $"todo:{id}";
+        var cacheDuration = TimeSpan.FromSeconds(20);
+        var cachedTodo = await _cache.GetAsync<Todo>(cacheKey);
+
+        if (cachedTodo is not null) return cachedTodo;
+
+        var todo = await _innerRepository.GetByIdAsync(id);
+
+        if (todo is not null)
+        {
+            await _cache.SetAsync(cacheKey, todo, cacheDuration);
+        }
+
+        return todo;
     }
 
     public async Task<PaginatedResult<Todo>> GetAllAsync(Guid? cursor, int? limit)
     {
-        return await _innerRepository.GetAllAsync(cursor, limit);
+        var cacheKey = $"todo:all";
+        var cacheDuration = TimeSpan.FromMinutes(1);
+        var cachedTodos = await _cache.GetAsync<PaginatedResult<Todo>>(cacheKey);
+
+        if (cachedTodos is not null) return cachedTodos;
+
+        var todos = await _innerRepository.GetAllAsync(cursor, limit);
+
+        await _cache.SetAsync(cacheKey, todos, cacheDuration);
+
+        return todos;
     }
 
     public Todo Update(Todo entity)
@@ -34,7 +56,15 @@ public class CachedTodoRepository : ITodoRepository
 
     public async Task<bool> DeleteAsync(Guid id)
     {
-        return await _innerRepository.DeleteAsync(id);
+        var cacheKey = $"todo:{id}";
+        bool deleted = await _innerRepository.DeleteAsync(id);
+
+        if (deleted)
+        {
+            await _cache.DeleteAsync(cacheKey);
+        }
+
+        return deleted;
     }
 
     public async Task<CategoryDetails> GetAllTodoCategories()
