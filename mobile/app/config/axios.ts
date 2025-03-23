aea38c1db82c658aa4208e6bcba94c9f16ae4152
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2025-03-23 12:08:29 +0100

    chore: add axios config, added tanstack query

--- mobile/app/config/axios.ts
+++ mobile/app/config/axios.ts
@@ -1,1 +1,10 @@
 import axios from "axios";
+
+const Axios = axios.create({
+  baseURL: process.env.EXPO_PUBLIC_API_URL as string,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default Axios;
